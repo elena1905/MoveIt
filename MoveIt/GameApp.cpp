4e2f83c224281328281014ee7289ffa1b27a0655
Wenchong Chen <wenchong.chen@outlook.com>
2012-11-06 22:13:17 +0000

    Kinect: skeleton tracking

--- MoveIt/GameApp.cpp
+++ MoveIt/GameApp.cpp
@@ -8,6 +8,7 @@
 
 #include "GameApp.h"
 
+/*
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
@@ -43,3 +44,4 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+*/
